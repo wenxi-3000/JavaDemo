3167511080cf94ba9a733553bcf1cd240288afe0
ShadowFlow <shadowflow@ShadowOS.local>
2022-04-11 17:43:38 +0800

    first commit

---
+++ ssm/src/main/java/com/shadowflow/service/impl/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package com.shadowflow.service.impl;
+
+import com.shadowflow.dao.AccountDao;
+import com.shadowflow.domain.Account;
+import com.shadowflow.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService{
+
+    @Autowired
+    private AccountDao accountDao;
+
+    public List<Account> findAll() {
+        System.out.println("业务层：查询所有账户...");
+        return accountDao.findAll();
+    }
+
+    public void saveAccount(Account account) {
+        System.out.println("业务层：保存帐户...");
+        accountDao.saveAccount(account);
+    }
+}
